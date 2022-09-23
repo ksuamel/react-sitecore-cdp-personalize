6ec64cc83ccefb7703db4ff4baf5016e037c34e7
Kevin Suarez <kevin.suarez.melendez@gmail.com>
2022-09-23 17:17:51 -0400

    Begin implementing server-side support

--- src/cdp-personalize.ts
+++ src/cdp-personalize.ts
@@ -1,90 +1,10 @@
-type boxeverQueueFunctionType = () => void;
-
-interface eventCreateResponse {
-  status: string;
-}
-
-type eventCreateCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  response: eventCreateResponse
-) => void;
-
-type callFlowsCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  response: unknown
-) => void;
-
-interface browserCreateResponse {
-  ref: string;
-}
-
-type browserCreateCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  data: browserCreateResponse
-) => void;
-
-interface browserShowResponse {
-  customer: Record<string, unknown>;
-}
-
-type browserShowCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  data: browserShowResponse
-) => void;
-
-interface Boxever {
-  getID(): string;
-  eventCreate(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: eventCreateCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    format: string
-  ): void;
-  callFlows(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: callFlowsCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    type: string
-  ): void;
-  storage: {
-    removeItem(
-      // eslint-disable-next-line no-unused-vars
-      name: string
-    ): void;
-    setItem(
-      // eslint-disable-next-line no-unused-vars
-      name: string,
-      // eslint-disable-next-line no-unused-vars
-      browserId: string,
-      // eslint-disable-next-line no-unused-vars
-      cookieExpiresDays: number | { TTL: number }
-    ): void;
-  };
-  cookie_name: string;
-  browserCreate(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: browserCreateCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    type: string
-  ): void;
-  browserShow(
-    browserId: string,
-    clientKey: string,
-    callback: browserShowCallbackType,
-    format: string
-  ): void;
-  browser_id: string;
-  isITPBrowser: boolean;
-  cookie_expires_days: number;
-  storage_ttl: number;
-  initWebFlowSDK(): void;
-}
+import { logError } from './logger';
+import {
+  Boxever,
+  BoxeverQueueFunctionType,
+  BrowserShowResponse,
+  EventCreateResponse,
+} from './types';
 
 declare global {
   interface Window {
@@ -95,7 +15,7 @@ declare global {
     };
     Boxever: Boxever;
     _boxever: Boxever;
-    _boxeverq: boxeverQueueFunctionType[];
+    _boxeverq: BoxeverQueueFunctionType[];
     __boxeverQueue: {
       new (): [];
     };
@@ -117,10 +37,6 @@ export class CdpPersonalize {
   private language: string = 'EN';
 
   private scriptId: string = 'cdpPersonalizeScript';
-
-  constructor() {
-    console.log('Initialized');
-  }
 
   initialize = (
     clientKey: string,
@@ -154,7 +70,7 @@ export class CdpPersonalize {
 
   renderScript = (): void => {
     if (!this.isInitialized) {
-      this.error('Initialize before attempting to render the script.');
+      logError('Initialize before attempting to render the script.');
     }
 
     const scriptAlreadyRendered = document.getElementById(this.scriptId);
@@ -188,7 +104,7 @@ export class CdpPersonalize {
     document.body.appendChild(script());
   };
 
-  trackPage = (): Promise<eventCreateResponse> => {
+  trackPage = (): Promise<EventCreateResponse> => {
     return this.eventCreate({
       type: 'VIEW',
     });
@@ -212,7 +128,7 @@ export class CdpPersonalize {
   identifyByEmail = (
     email: string,
     additionalData?: Record<string, unknown>
-  ): Promise<eventCreateResponse> => {
+  ): Promise<EventCreateResponse> => {
     const finalPayload = {
       email: email,
       ...additionalData,
@@ -224,7 +140,7 @@ export class CdpPersonalize {
     provider: string,
     id: string,
     additionalData?: Record<string, unknown>
-  ): Promise<eventCreateResponse> => {
+  ): Promise<EventCreateResponse> => {
     const identifyEvent: Record<string, unknown> = {
       type: 'IDENTITY',
       identifiers: [
@@ -245,8 +161,8 @@ export class CdpPersonalize {
 
   eventCreate = (
     event: Record<string, unknown>
-  ): Promise<eventCreateResponse> => {
-    return new Promise<eventCreateResponse>(resolve => {
+  ): Promise<EventCreateResponse> => {
+    return new Promise<EventCreateResponse>(resolve => {
       this.waitForBoxever().then(async () => {
         const eventPayload = {
           page: window.location.pathname + window.location.search,
@@ -269,8 +185,8 @@ export class CdpPersonalize {
     });
   };
 
-  browserShow = async (browserId?: string): Promise<browserShowResponse> => {
-    return new Promise<browserShowResponse>(resolve => {
+  browserShow = async (browserId?: string): Promise<BrowserShowResponse> => {
+    return new Promise<BrowserShowResponse>(resolve => {
       this.waitForBoxever().then(async () => {
         const bid = browserId ?? (await this.getBrowserId());
         window.Boxever.browserShow(
@@ -285,27 +201,27 @@ export class CdpPersonalize {
 
   private isInitialized = (): boolean => {
     if (!this.clientKey || this.clientKey.length === 0) {
-      this.error('Missing Client Key');
+      logError('Missing Client Key');
       return false;
     }
 
     if (!this.targetApi || this.targetApi.length === 0) {
-      this.error('Missing Target API');
+      logError('Missing Target API');
       return false;
     }
 
     if (!this.cookieDomain || this.cookieDomain.length === 0) {
-      this.error('Missing Cookie Domain');
+      logError('Missing Cookie Domain');
       return false;
     }
 
     if (!this.pointOfSale || this.pointOfSale.length === 0) {
-      this.error('Missing Point of Sale');
+      logError('Missing Point of Sale');
       return false;
     }
 
     if (!this.webFlowTarget || this.webFlowTarget.length === 0) {
-      this.error('Missing Web Flow Target');
+      logError('Missing Web Flow Target');
       return false;
     }
 
@@ -319,15 +235,10 @@ export class CdpPersonalize {
         window.Boxever.getID() === 'anonymous' ||
         !window._boxeverq
       ) {
-        console.log(`Boxever is not ready yet. Waiting 100ms before retrying.`);
-        await new Promise(r => setTimeout(r, 100));
+        await new Promise(r => setTimeout(r, 150));
       }
 
       resolve();
     });
-  };
-
-  private error = (message: string) => {
-    console.error(`react-sitecore-cdp-personalize: ${message}`);
   };
 }
---
+++ src/types.ts
@@ -0,0 +1,60 @@
+export type BoxeverQueueFunctionType = () => void;
+
+export interface EventCreateResponse {
+  status: string;
+}
+
+type EventCreateCallbackType = (response: EventCreateResponse) => void;
+
+type CallFlowsCallbackType = (response: unknown) => void;
+
+interface BrowserCreateResponse {
+  ref: string;
+}
+
+type BrowserCreateCallbackType = (data: BrowserCreateResponse) => void;
+
+export interface BrowserShowResponse {
+  customer: Record<string, unknown>;
+}
+
+type BrowserShowCallbackType = (data: BrowserShowResponse) => void;
+
+export interface Boxever {
+  getID(): string;
+  eventCreate(
+    payload: Record<string, unknown>,
+    callback: EventCreateCallbackType,
+    format: string
+  ): void;
+  callFlows(
+    payload: Record<string, unknown>,
+    callback: CallFlowsCallbackType,
+    type: string
+  ): void;
+  storage: {
+    removeItem(name: string): void;
+    setItem(
+      name: string,
+      browserId: string,
+      cookieExpiresDays: number | { TTL: number }
+    ): void;
+  };
+  cookie_name: string;
+  browserCreate(
+    payload: Record<string, unknown>,
+    callback: BrowserCreateCallbackType,
+    type: string
+  ): void;
+  browserShow(
+    browserId: string,
+    clientKey: string,
+    callback: BrowserShowCallbackType,
+    format: string
+  ): void;
+  browser_id: string;
+  isITPBrowser: boolean;
+  cookie_expires_days: number;
+  storage_ttl: number;
+  initWebFlowSDK(): void;
+}
