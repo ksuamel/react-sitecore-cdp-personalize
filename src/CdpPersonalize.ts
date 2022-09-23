2dac2c9c63e72dd4bd2cecf8d0214c8c681a4ee2
Kevin Suarez <kevin.suarez.melendez@gmail.com>
2022-09-23 14:22:03 -0400

    Update ts-eslint version

--- src/CdpPersonalize.ts
+++ src/CdpPersonalize.ts
@@ -1,3 +1,110 @@
+type boxeverQueueFunctionType = () => void;
+
+interface eventCreateResponse {
+  status: string;
+}
+
+type eventCreateCallbackType = (
+  // eslint-disable-next-line no-unused-vars
+  response: eventCreateResponse
+) => void;
+
+type callFlowsCallbackType = (
+  // eslint-disable-next-line no-unused-vars
+  response: unknown
+) => void;
+
+interface browserCreateResponse {
+  ref: string;
+}
+
+type browserCreateCallbackType = (
+  // eslint-disable-next-line no-unused-vars
+  data: browserCreateResponse
+) => void;
+
+interface browserShowResponse {
+  customer: Record<string, unknown>;
+}
+
+type browserShowCallbackType = (
+  // eslint-disable-next-line no-unused-vars
+  data: browserShowResponse
+) => void;
+
+interface Boxever {
+  getID(): string;
+  eventCreate(
+    // eslint-disable-next-line no-unused-vars
+    payload: Record<string, unknown>,
+    // eslint-disable-next-line no-unused-vars
+    callback: eventCreateCallbackType,
+    // eslint-disable-next-line no-unused-vars
+    format: string
+  ): void;
+  callFlows(
+    // eslint-disable-next-line no-unused-vars
+    payload: Record<string, unknown>,
+    // eslint-disable-next-line no-unused-vars
+    callback: callFlowsCallbackType,
+    // eslint-disable-next-line no-unused-vars
+    type: string
+  ): void;
+  storage: {
+    removeItem(
+      // eslint-disable-next-line no-unused-vars
+      name: string
+    ): void;
+    setItem(
+      // eslint-disable-next-line no-unused-vars
+      name: string,
+      // eslint-disable-next-line no-unused-vars
+      browserId: string,
+      // eslint-disable-next-line no-unused-vars
+      cookieExpiresDays: number | { TTL: number }
+    ): void;
+  };
+  cookie_name: string;
+  browserCreate(
+    // eslint-disable-next-line no-unused-vars
+    payload: Record<string, unknown>,
+    // eslint-disable-next-line no-unused-vars
+    callback: browserCreateCallbackType,
+    // eslint-disable-next-line no-unused-vars
+    type: string
+  ): void;
+  browserShow(
+    browserId: string,
+    clientKey: string,
+    callback: browserShowCallbackType,
+    format: string
+  ): void;
+  browser_id: string;
+  isITPBrowser: boolean;
+  cookie_expires_days: number;
+  storage_ttl: number;
+  initWebFlowSDK(): void;
+}
+
+declare global {
+  interface Window {
+    _boxever_settings: {
+      client_key: string;
+      target: string;
+      cookie_domain: string;
+    };
+    Boxever: Boxever;
+    _boxever: Boxever;
+    _boxeverq: boxeverQueueFunctionType[];
+    __boxeverQueue: {
+      new (): [];
+    };
+    BoxeverJERS: {
+      errors: unknown[];
+    };
+  }
+}
+
 export class CdpPersonalize {
   private clientKey!: string;
   private targetApi!: string;
@@ -139,111 +246,4 @@ export class CdpPersonalize {
 
   private getCookieValue = (name: string) =>
     document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '';
-}
-
-type boxeverQueueFunctionType = () => void;
-
-interface eventCreateResponse {
-  status: string;
-}
-
-type eventCreateCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  response: eventCreateResponse
-) => void;
-
-type callFlowsCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  response: unknown
-) => void;
-
-interface browserCreateResponse {
-  ref: string;
-}
-
-type browserCreateCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  data: browserCreateResponse
-) => void;
-
-interface browserShowResponse {
-  customer: Record<string, unknown>;
-}
-
-type browserShowCallbackType = (
-  // eslint-disable-next-line no-unused-vars
-  data: browserShowResponse
-) => void;
-
-interface Boxever {
-  getID(): string;
-  eventCreate(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: eventCreateCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    format: string
-  ): void;
-  callFlows(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: callFlowsCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    type: string
-  ): void;
-  storage: {
-    removeItem(
-      // eslint-disable-next-line no-unused-vars
-      name: string
-    ): void;
-    setItem(
-      // eslint-disable-next-line no-unused-vars
-      name: string,
-      // eslint-disable-next-line no-unused-vars
-      browserId: string,
-      // eslint-disable-next-line no-unused-vars
-      cookieExpiresDays: number | { TTL: number }
-    ): void;
-  };
-  cookie_name: string;
-  browserCreate(
-    // eslint-disable-next-line no-unused-vars
-    payload: Record<string, unknown>,
-    // eslint-disable-next-line no-unused-vars
-    callback: browserCreateCallbackType,
-    // eslint-disable-next-line no-unused-vars
-    type: string
-  ): void;
-  browserShow(
-    browserId: string,
-    clientKey: string,
-    callback: browserShowCallbackType,
-    format: string
-  ): void;
-  browser_id: string;
-  isITPBrowser: boolean;
-  cookie_expires_days: number;
-  storage_ttl: number;
-  initWebFlowSDK(): void;
-}
-
-declare global {
-  interface Window {
-    _boxever_settings: {
-      client_key: string;
-      target: string;
-      cookie_domain: string;
-    };
-    Boxever: Boxever;
-    _boxever: Boxever;
-    _boxeverq: boxeverQueueFunctionType[];
-    __boxeverQueue: {
-      new (): [];
-    };
-    BoxeverJERS: {
-      errors: unknown[];
-    };
-  }
 }
